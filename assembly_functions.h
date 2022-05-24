488439d8adda95bb35ab0c6c2fd6c3899139bdc9
CJ-404 <cj.ptsz@yahoo.com>
2022-05-24 18:57:22 +0530

    added load(r) , store functions & completed main.c

--- assembly_functions.h
+++ assembly_functions.h
@@ -12,3 +12,20 @@ void movv(int to,int from){
     reg[to]=from;
 	reg[pc]=reg[pc]+1;
 }
+
+void load(int to,int from){
+    reg[to]=char_toint(memory[from]);
+	reg[pc]=reg[pc]+1;
+}
+	
+
+void loadr(int to,int from){
+    reg[to]=char_toint(memory[reg[from]]);
+	reg[pc]=reg[pc]+1;
+}
+	
+
+void store(int to,int from){
+    strcpy(memory[to],int_Tochar(reg[from]));
+	reg[pc]=reg[pc]+1;
+}
--- main.c
+++ main.c
@@ -10,7 +10,22 @@ Registers {a=0, b, c, d, e,f,sp,acc, pc, ivec, Int, timer, halt};     	   int --
 
 int main(){
 
+	// initialize the memory...
+    	initialize_memory(memory,30);
+    	initialize_reg();
+
+	//open & store the assembly file
+	FILE *fd = fopen("prog.asm","r");
+    	store_into(fd,memory);
+	fclose(fd);
+	
+	//run virtual machine
+	runvm();
+
+	print_memory(memory);
+	print_registers(reg);
 
 
+	printf("\nEnd of MY Virtual Machine\n");
 	return 0;
 }
